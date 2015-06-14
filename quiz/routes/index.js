144830d8dd67027458c47f7ec421cb13a1968e7c
javier.vaquero <javi_salamanca@hotmail.com>
2015-06-14 13:00:11 +0200

    Ejercicio tema 6

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -12,4 +12,8 @@ router.get('/quizes/question', quizController.question);
 
 router.get('/quizes/answer', quizController.answer);
 
+router.get('/author', function(req, res, next) {
+  res.render('author');
+});
+
 module.exports = router;
