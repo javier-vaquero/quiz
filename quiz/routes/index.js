6c5590fc552bde766ea590fa7a1664a0a8dc30d7
javier.vaquero <javi_salamanca@hotmail.com>
2015-08-02 15:41:15 +0200

    Autorizacion, moderacion de comentarios y HTTPS

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -21,6 +21,7 @@ router.get('/author', function(req, res, next) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load); // autoload :commentId
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.question);
@@ -28,20 +29,27 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 // Gestion:
 // Creacion
-router.get('/quizes/new', quizController.newQuiz);
-router.post('/quizes/create', quizController.create);
+router.get('/quizes/new', sessionController.loginRequired,
+		quizController.newQuiz);
+router.post('/quizes/create', sessionController.loginRequired,
+		quizController.create);
 // Modificacion
-router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
-router.post('/quizes/:quizId(\\d+)/update', quizController.update);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired,
+		quizController.edit);
+router.post('/quizes/:quizId(\\d+)/update', sessionController.loginRequired,
+		quizController.update);
 // Borrar
-router.get('/quizes/:quizId(\\d+)/delete', quizController.destroy);
+router.get('/quizes/:quizId(\\d+)/delete', sessionController.loginRequired,
+		quizController.destroy);
 // Comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.newComment);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+		sessionController.loginRequired, commentController.publish);
 // Sesiones
-//Definición de rutas de sesion
-router.get('/login',  sessionController.newSession);     // formulario login
-router.post('/login', sessionController.create);  // crear sesión
+// Definición de rutas de sesion
+router.get('/login', sessionController.newSession); // formulario login
+router.post('/login', sessionController.create); // crear sesión
 router.get('/logout', sessionController.destroy); // destruir sesión
 
 module.exports = router;
